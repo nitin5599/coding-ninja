60ea544391156ec8f33c4d716d6ecabf3391e5f7
nitin5599 <nitin5599>
2021-06-16 16:27:36 +0530

    code changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+// import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+
+import { NavbarComponent } from './front/navbar/navbar.component';
+import { HeaderComponent } from './front/header/header.component';
+import { FooterComponent } from './front/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgSelectModule,
+    // NgOptionHighlightModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/front/header/header.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  Tabs:any[] = [
+    {name:'All Events', value:'ALL_EVENTS', status:'active', show:'show'},
+    {name:'Webinars', value:'WEBINAR', status:'', show:''},
+    {name:'Coding Events', value:'CODING_EVENT', status:'', show:''},
+    {name:'Bootcamp Events', value:'BOOTCAMP_EVENT', status:'', show:''},
+    {name:'Workshop', value:'WORKSHOP', status:'', show:''},
+  ]
+
+  subCtg:any[] = [
+    {name:'All Time Favorites', value:'ALL_TIME_FAVORITES'},
+    {name:'Archived', value:'Archived'},
+    {name:'Upcoming', value:'Upcoming'},
+  ]
+
+  allTags: any[];
+	selectedTagIds: any = 'Competitive Programming';
+
+  tagsApi = 'https://api.codingninjas.com/api/v3/event_tags';
+  listApi = 'https://api.codingninjas.com/api/v3/events';
+  eventTag: any = 'ALL_EVENTS';
+  selectedSubCtgId: any = 'Upcoming';
+  
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any>(this.tagsApi).subscribe((data: any) => {
+          // console.log(data)
+          this.allTags = [...data.data.tags];
+      },
+      (error) => {
+          console.error('There was an error!', error);
+      }
+    )
+    this.showEventCards();
+  }
+
+  showEventCards(){
+    this.http.get<any>(this.listApi+'?event_category='+this.eventTag+'&event_sub_category='+this.selectedSubCtgId+'&tag_list='+this.selectedTagIds+'&offset=0').subscribe((data: any) =>{
+      console.log(data);
+    })
+  }
+
+  showActiveTab(tag: any){
+    this.eventTag = tag;
+    this.showEventCards();
+  }
+
+  selectedTag(){
+    console.log(this.selectedTagIds);
+    this.showEventCards();
+  }
+  
+  selectedSubCtg(){
+    console.log('ctg -', this.selectedSubCtgId)
+    this.showEventCards();
+  }
+
+  onSelectAll() {
+		this.selectedTagIds = this.allTags;
+    this.selectedTag();
+	}
+
+	onClearAll() {
+		this.selectedTagIds = [];
+    this.selectedTag();
+	}
+
+}
